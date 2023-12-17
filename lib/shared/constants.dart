e82a7f45bc0a8d38e8c9672c0449aa1e5f5932eb
innomatic <innomatic@innomatic.ca>
2023-12-17 10:49:03 -0500

    version:2.5.6+44

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,7 +1,7 @@
 // app info
 const appId = 'com.innomatic.cartaapp';
 const appName = 'Carta';
-const appVersion = '2.5.5+43';
+const appVersion = '2.5.6+44';
 const emailDeveloper = 'nuntium.ubique@gmail.com';
 const urlGooglePlay = 'https://play.google.com/store/apps/details?id=$appId';
 const urlAppStore = null;
